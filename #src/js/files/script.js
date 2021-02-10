467d1e92a90a67daefb9c9fd0f2423f77e6e951a
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2021-02-10 14:14:29 +0200

    added section slider

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,5 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const sliderSwiper = new Swiper('.slider__body', {
+        loop: true,
+        slidesPerView: 'auto',
+        loopedSlides: 10,
+      });
 
-});
+}); // end
